7bef407814bfa2eac4018abc630146653eb88fc7
Maksymenko Danylo <danilmaksimenko000@gmail.com>
2026-03-21 13:51:12 +0200

    implement dynamic type select

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,22 @@
-console.log('start')
+const merchantTypes = [
+  { value: 'atm', label: 'Банкомат' },
+  { value: 'tso', label: 'ТСО' }
+];
+
+const merchantTypeSelect = document.getElementById('merchantType');
+
+function renderMerchantTypes() {
+  if(!merchantTypeSelect) {
+    return;
+  }
+
+  merchantTypes.forEach((type) => {
+    const optionElement = document.createElement('option');
+    optionElement.value = type.value;
+    optionElement.textContent = type.label;
+
+    merchantTypeSelect.append(optionElement);
+  })
+}
+
+renderMerchantTypes();
